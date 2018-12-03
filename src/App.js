8688cdb73eedb5c78cd80d1918478a90b8faac7b
ShuyangYu <yushuyanggyx@163.com>
2018-12-03 21:17:55 +0800

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import Header from './common/header';
 import { Provider } from 'react-redux';
 import { GlobalIconfontStyle}  from './statics/iconfont/iconfont';
 import store from './store';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <Provider store={ store }>
-          <Header />
+          <div>
+            <Header />
+            <BrowserRouter> 
+              <div>
+                <Route path='/' exact render={() => <div>home</div>}></Route>
+                <Route path='/detail' exact render={() => <div>detail</div>}></Route>
+              </div>
+            </BrowserRouter>
+          </div>
         </Provider>
         
         <GlobalIconfontStyle />
