f3a94c0a404158989b8f75fb4ebef9094687902c
ShuyangYu <yushuyanggyx@163.com>
2018-12-03 22:14:23 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from './common/header';
 import { Provider } from 'react-redux';
 import { GlobalIconfontStyle}  from './statics/iconfont/iconfont';
 import store from './store';
+import Home from './pages/home';
+import Detail from './pages/detail';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
@@ -14,8 +16,8 @@ class App extends Component {
             <Header />
             <BrowserRouter> 
               <div>
-                <Route path='/' exact render={() => <div>home</div>}></Route>
-                <Route path='/detail' exact render={() => <div>detail</div>}></Route>
+                <Route path='/' exact component={Home}></Route>
+                <Route path='/detail' exact component={Detail}></Route>
               </div>
             </BrowserRouter>
           </div>
